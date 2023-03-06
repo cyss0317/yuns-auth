07b32e61a2e1facdef155dfcc6bc63455abb071e
Yun Choi <cyss0317@gmail.com>
2023-03-05 22:36:41 -0600

    Created session controller

---
+++ config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "https://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :head, :options], credential: true
+    # credential: true makes it possible for headers to be passed
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :sessions, only: [:create]
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "static#home"
 end
