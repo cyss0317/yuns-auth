6d213d4411c6791dacdbea8d35b7e8a31896ebca
Yun Choi <cyss0317@gmail.com>
2023-03-09 15:49:37 -0600

    Updated Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,13 +2,19 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost:3000'
+    origins 'http://localhost:3000'
     resource '*', headers: :any, methods: %i[get post put patch delete head options], credential: true
     # credential: true makes it possible for headers to be passed
   end
 
   allow do
-    origins 'https://localhost:5174'
+    origins 'http://localhost:5174'
+    resource '*', headers: :any, methods: %i[get post put patch delete head options], credential: true
+    # credential: true makes it possible for headers to be passed
+  end
+
+  allow do
+    origins 'http://localhost:5173'
     resource '*', headers: :any, methods: %i[get post put patch delete head options], credential: true
     # credential: true makes it possible for headers to be passed
   end
